1da42839e42573dae4215d1ccabd2bcc9b1944d8
Vicente Rivera <vicenterivera225@gmail.com>
2023-11-21 13:52:41 -0300

    chao php, hola astro

--- scripts/contact.js
+++
@@ -1,8 +0,0 @@
-document.getElementById("contact-form").addEventListener("submit", alertForm);
-function alertForm(event) {
-    event.preventDefault();
-    const name = document.getElementById("name").value;
-    const email = document.getElementById("email").value;
-    const message = document.getElementById("message").value;
-    alert(`Name: ${name} \nEmail: ${email} \nMessage: ${message}`);
-}
--- scripts/index.js
+++
@@ -1,7 +0,0 @@
-document.getElementById("logo").addEventListener("mouseover", function() {
-    this.style.transform = "scale(1.5)";
-});
-
-document.getElementById("logo").addEventListener("mouseout", function() {
-    this.style.transform = "scale(1)";
-});
